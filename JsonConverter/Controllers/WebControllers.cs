473e5d35eb2f9033259237ab7e0b2efb416cbf4d
E-Mauricio <dokito_68@outlook.com>
2024-02-13 03:05:02 -0600

    Add project files.

---
+++ JsonConverter/Controllers/WebControllers.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace JsonConverter.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class WebControllers : Controller
+    {
+        [HttpPost]
+        public IActionResult UploadFile(string nameTest)
+        {
+            //Chamba del codigo del controller que hace lo del Excel.
+
+            //var result = await sonConvert.DeserializeObject<WeatherForecast>(json);
+
+            //TEST
+            var word = nameTest + " TEST " + nameTest;
+
+            return Ok(word);
+            
+        }
+    }
+}
---
+++ JsonConverter/Program.cs
@@ -0,0 +1,49 @@
+namespace JsonConverter
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            var builderConfiguration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddEnvironmentVariables();
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            builder.Services.AddAuthorization();
+
+            builder.Services.AddSingleton<IConfiguration>(builderConfiguration.Build());
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseStaticFiles();
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseStaticFiles();
+
+            app.UseCors(policy => policy.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin());
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
